9b34585476ed0ccca6789690080b5ac7b66b99c9
Mike Castro <mkcastro@gmail.com>
2020-12-16 21:36:53 +0800

    first

--- main.py
+++ main.py
@@ -1,19 +1,59 @@
 # %%
+import datetime
+import os
+
 import pikepdf
 import tabula
 
 
 # %%
+def get_latest_file_unencrypted_file():
+    encrypted = get_latest_file("./encrypted/")
+    unencrypted = get_latest_file("./unencrypted/")
+    current_date_and_time = datetime.datetime.now()
+    current_date_and_time_string = str(current_date_and_time)
+    extension = ".csv"
+    unencrypted_filename = current_date_and_time_string + extension
+
+    if encrypted == unencrypted:
+        decrypt(encrypted, "password", unencrypted_filename)
+
+    return unencrypted_filename
+
+
+def get_latest_file(directory):
+    return os.listdir()
+
+
 def decrypt(input_filename, password, output_filename):
     pdf = pikepdf.open(input_filename, password=password)
     pdf.save(output_filename)
 
 
-# %%
-filename = "unencrypted/1.pdf"
-df = tabula.read_pdf(filename, pages="1-10")
-df
+def convert_pdf(filename):
+    return tabula.read_pdf(filename, pages="1-10")
 
-# %%
-if __name__ == "__main__":
+
+def convert_dataframe_to_workable_data():
     pass
+
+
+def merge_page_breaks():
+    pass
+
+
+def display():
+    pass
+
+
+def main():
+    filename = get_latest_file_unencrypted_file()
+    filename
+    # dataframe = convert_pdf(filename)
+    # convert_dataframe_to_workable_data()
+    # merge_page_breaks()
+    # display()
+
+
+if __name__ == "__main__":
+    main()
